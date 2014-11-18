bec128eb314749d4fe6c5cd3a15edc09c5b821ba
pvergara <pablocristobal@gmail.com>
2014-11-18 21:38:37 +0100

    First version of InitData

--- Ecos.DBInit.MySql/MySqlDBInit.cs
+++ Ecos.DBInit.MySql/MySqlDBInit.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using Ecos.DBInit.Core.ScriptHelpers;
 using Ecos.DBInit.Core.Model;
 using MySql.Data.MySqlClient;
+using Ecos.DBInit.MySql.ScriptHelpers;
+using System.Data;
+using System.Linq;
 
 namespace Ecos.DBInit.MySql
 {
@@ -10,14 +13,90 @@ namespace Ecos.DBInit.MySql
 	{
         readonly string _assemblyName;
         readonly MySqlConnection _connection;
+        readonly List<Script> _scripts = new List<Script>();
+        readonly ICollection<Script> _dropAllSchemaObjectsScripts = new List<Script>();
+        readonly MySqlScriptHelper _helper;
+        readonly ICollection<string> _tableNames = new List<string>();
 
         public MySqlDBInit(string connectionString,string assemblyName){
             _assemblyName = assemblyName;
+            //TODO: WHAT ABOUT THE INJECTION!!!!
             _connection = new MySqlConnection(connectionString);
+            //TODO: WHAT ABOUT THE INJECTION!!!!
+            _helper = new MySqlScriptHelper(connectionString);
         }
 
         public void InitData()
         {
+            CleanData();
+            AddData();
+            ExecuteFromUoW();
+        }
+
+        void CleanData()
+        {
+            DeactivateReferentialIntegrity();
+            CleanEachTable();
+        }
+
+        void CleanEachTable()
+        {
+            var scripts = new List<Script>();
+
+            scripts.AddRange(GetDeleteEachTableScripts());
+
+            AddToUoW(scripts);
+        }
+
+        IEnumerable<Script> GetDeleteEachTableScripts()
+        {
+            IEnumerable<Script> enumerable = GetEachTableName().
+                Select(tableName => Script.From(string.Format("DELETE FROM {0};", tableName)));
+            return enumerable;
+        }
+
+        IEnumerable<string> GetEachTableName()
+        {
+            var container = ScriptFinderFluentFactory.
+                FromEmbeddedResource.
+                InitWith(GetType().Assembly.GetName().Name, ScriptType.Data).
+                GetContainer();
+
+            var scriptsWithTableNames = ScriptLoaderFluentFactory.
+                FromEmbeddedResource.
+                InitWith(GetType().Assembly.GetName().Name, container).
+                GetScripts("_DATABASE_NAME_",_connection.Database);
+
+            IDictionary<int, Script> scriptMap = scriptsWithTableNames.Select((s, i) => new { s, i }).ToDictionary(x => x.i, x => x.s);
+            _helper.ExecuteAndProcess(scriptMap,AddFirstFieldIntoStringCollectionField);
+            return _tableNames;
+        }
+            
+        void AddFirstFieldIntoStringCollectionField(IDataRecord reader,int index){
+            _tableNames.Add(reader.GetString(0));
+        }
+
+        void AddData()
+        {
+            LoadDataScripts();
+            ActivateReferentialIntegrity();
+        }
+
+        void LoadDataScripts()
+        {
+            var container = 
+                ScriptFinderFluentFactory.
+                FromEmbeddedResource.
+                InitWith(_assemblyName, ScriptType.Data).
+                GetContainer();
+
+            var scripts = 
+                ScriptLoaderFluentFactory.
+                FromEmbeddedResource.
+                InitWith(_assemblyName, container).
+                GetScripts();
+
+            AddToUoW(scripts);
         }
 
         public void InitSchema()
@@ -26,22 +105,11 @@ namespace Ecos.DBInit.MySql
             InitializeDB();
             ExecuteFromUoW();
         }
-
-
+            
         private void CleanDB()
         {
             DeactivateReferentialIntegrity();
             DropDataBaseObjects();
-        }
-
-        void ExecuteFromUoW()
-        {
-            foreach (var sql in _scripts)
-            {
-                var script = new MySqlScript(_connection, sql.Query);
-                script.Execute();
-            }
-            _scripts.Clear();
         }
 
         private void InitializeDB()
@@ -69,7 +137,14 @@ namespace Ecos.DBInit.MySql
         IEnumerable<Script> LoadDropAllSchemaObjectsScripts()
         {
             IEnumerable<Script> scriptsWithDropFunction = ComposeScriptsWithDroptAllSchemaObjects();
-            return  ExecuteAndReturnFirstField(scriptsWithDropFunction);
+            IDictionary<int, Script> dictionary = scriptsWithDropFunction.Select((s, i) => new { s, i }).ToDictionary(x => x.i, x => x.s);
+            _helper.ExecuteAndProcess(dictionary,AddFirstFieldIntoScriptMap);
+            return _dropAllSchemaObjectsScripts;
+        }
+
+        void AddFirstFieldIntoScriptMap(IDataRecord reader, int index)
+        {
+            _dropAllSchemaObjectsScripts.Add(Script.From(reader.GetString(0)));
         }
 
         IEnumerable<Script> ComposeScriptsWithDroptAllSchemaObjects()
@@ -84,8 +159,7 @@ namespace Ecos.DBInit.MySql
                     InitWith(GetType().Assembly.GetName().Name, container).
                 GetScripts("_DATABASE_NAME_",_connection.Database);
         }
-
-
+            
         void ActivateReferentialIntegrity()
         {
             var scripts = new []{ Script.From("SET @@foreign_key_checks = 1;") };
@@ -110,31 +184,16 @@ namespace Ecos.DBInit.MySql
             AddToUoW(scripts);
         }
 
-        List<Script> _scripts = new List<Script>();
-
         void AddToUoW(IEnumerable<Script> scripts)
         {
             _scripts.AddRange(scripts);
         }
 
-
-        IEnumerable<Script> ExecuteAndReturnFirstField(IEnumerable<Script> scripts)
+        void ExecuteFromUoW()
         {
-            var result = new List<Script>();
-            foreach (Script script in scripts)
-            {
-                _connection.Open();
-                var command = new MySqlCommand(script.Query,_connection);
-                var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    result.Add(Script.From(reader.GetString(0)));
-                }
-                _connection.Close();
-            }
-            return result;
+            _helper.Execute(_scripts);
+            _scripts.Clear();
         }
-
 	}
 
 }
--- Ecos.DBInit.Test/DBInitTest.cs
+++ Ecos.DBInit.Test/DBInitTest.cs
@@ -1,8 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Data.Common;
 using Ecos.DBInit.Core.Model;
 using Ecos.DBInit.Wire;
+using Ecos.DBInit.MySql.ScriptHelpers;
 
 namespace Ecos.DBInit.Test
 {
@@ -10,8 +10,12 @@ namespace Ecos.DBInit.Test
 	public class DBInitTest
 	{
         const string DBName = "sakila";
+        const string AssemblyName = "Ecos.DBInit.Samples.ProjectWithAMySQLDataBase";
+        const string QueryToKnowNumberOfRowsOfActorsTable = "SELECT count(*) FROM "+DBName+".actor;";
+        const string QueryToKnowNumberOfRowsOfAddressTable = "SELECT count(*) FROM "+DBName+".address;";
 
         readonly string _connectionString;
+        readonly MySqlScriptHelper _helper;
 
         public DBInitTest(){
             _connectionString =
@@ -20,23 +24,12 @@ namespace Ecos.DBInit.Test
                 "User ID=desarrollo;" +
                 "Password=3QSo5cff;" +
                 "Allow User Variables=True";
+            _helper = new MySqlScriptHelper(_connectionString);
         }
 
 		long ExecScalarByUsing (string sqlCommand)
 		{
-            const string providerInvariantName = "MySql.Data.MySqlClient";
-			long result;
-		
-			var dbProviderFactory = DbProviderFactories.GetFactory (providerInvariantName);
-			var dbcon = dbProviderFactory.CreateConnection ();
-			dbcon.ConnectionString = _connectionString;
-			dbcon.Open ();
-			using (var dbcmd = dbcon.CreateCommand ()) {
-				dbcmd.CommandText = sqlCommand;
-				result = (long)dbcmd.ExecuteScalar ();
-			}
-			dbcon.Close ();
-			return result;
+            return _helper.ExecuteScalar<long>(Script.From(sqlCommand));			
 		}
 
 		[Test]
@@ -45,12 +38,10 @@ namespace Ecos.DBInit.Test
 			//Arrange			
 			const string queryToKnowNumberOfTablesAndViews = "SELECT count(*) FROM information_schema.tables WHERE table_schema = '"+DBName+"';";
 			const string queryToKnowNumberOfStoredProceduresAndFunctions = "SELECT count(*) FROM information_schema.routines WHERE routine_schema = '"+DBName+"';";
-            const string queryToKnowNumberOfRowsOfActorsTable = "SELECT count(*) FROM "+DBName+".actor;";
 
-            const string assemblyName = "Ecos.DBInit.Samples.ProjectWithAMySQLDataBase";
             var dbInit = DBInitFactory.
                 From(ProviderType.MySql).
-                    InitWith(_connectionString, assemblyName).
+                    InitWith(_connectionString, AssemblyName).
                 GetDBInit();
 
 			//Act
@@ -59,12 +50,30 @@ namespace Ecos.DBInit.Test
 			//Pre-Assert
 			var numberOfTablesAndViews = ExecScalarByUsing (queryToKnowNumberOfTablesAndViews);
 			var numberOfStoredProcecuresAndFunctions = ExecScalarByUsing (queryToKnowNumberOfStoredProceduresAndFunctions);
-			var numberOfKnowNumberOfRowsOfActorsTable = ExecScalarByUsing (queryToKnowNumberOfRowsOfActorsTable);
+			var numberOfKnowNumberOfRowsOfActorsTable = ExecScalarByUsing (QueryToKnowNumberOfRowsOfActorsTable);
 
 			//Assert
 			Assert.That (numberOfTablesAndViews, Is.EqualTo (23));
 			Assert.That (numberOfStoredProcecuresAndFunctions, Is.EqualTo (6));
 			Assert.That (numberOfKnowNumberOfRowsOfActorsTable, Is.EqualTo (0));
 		}			
+
+
+        [Test]
+        public void WhenIUseInitDataTheSystemWillAddAllDataIntoSchema ()
+        {
+            //Arrange           
+            var dbInit = DBInitFactory.
+                From(ProviderType.MySql).
+                    InitWith(_connectionString, AssemblyName).
+                GetDBInit();
+
+            //Act
+            dbInit.InitData ();
+
+            //Assert
+            Assert.That (ExecScalarByUsing(QueryToKnowNumberOfRowsOfActorsTable), Is.EqualTo (200));
+            Assert.That (ExecScalarByUsing(QueryToKnowNumberOfRowsOfAddressTable), Is.EqualTo (603));
+        }
 	}
 }
