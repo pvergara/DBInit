093bd6e80554eafa7cf62f4e4ae511b0a5f3bdc1
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2014-11-27 11:50:03 +0100

    I forgot the tests :-(

--- Ecos.DBInit.Test/MySqlOperator/MySqlSpecificDBOperatorTest.cs
+++ Ecos.DBInit.Test/MySqlOperator/MySqlSpecificDBOperatorTest.cs
@@ -12,24 +12,28 @@ namespace Ecos.DBInit.Test.MySqlOperator
     {
         private readonly ISpecificDBOperator _specificDbOperation;
 
-        public MySqlSpecificDBOperatorTest(){
+        public MySqlSpecificDBOperatorTest()
+        {
             _specificDbOperation = new SpecificDBOperator();
         }
 
         [Test]
-        public void ComposeScriptsDelete(){
+        public void ComposeScriptsDelete()
+        {
             //Arrange
-            var tableNames = new[] {"table1","table2","table3"};
+            var tableNames = new[] { "table1", "table2", "table3" };
 
             //Action
             var deleteScript = _specificDbOperation.ComposeScriptsDelete(tableNames);
 
             //Asserts
-            Assert.That(deleteScript, Is.SubsetOf(tableNames.Select(table =>Script.From(string.Format("DELETE FROM {0};", table)))));
+            Assert.That(deleteScript, Is.SubsetOf(tableNames.Select(table => Script.From(string.Format("DELETE FROM {0};", table)))));
+            Assert.That(deleteScript.ToList(), Has.Count.EqualTo(tableNames.Count()));
         }
 
         [Test]
-        public void ComposeActivateReferentialIntegrity(){
+        public void ComposeActivateReferentialIntegrity()
+        {
             //Arrange
 
             //Action
@@ -37,6 +41,74 @@ namespace Ecos.DBInit.Test.MySqlOperator
 
             //Asserts
             Assert.That(activateReferentialIntegrityScript, Is.EqualTo(Script.From("SET @@foreign_key_checks = 1;")));
+        }
+
+        [Test]
+        public void ComposeDeactivateReferentialIntegrity()
+        {
+            //Arrange
+
+            //Action
+            var activateReferentialIntegrityScript = _specificDbOperation.ComposeDeactivateReferentialIntegrity();
+
+            //Asserts
+            Assert.That(activateReferentialIntegrityScript, Is.EqualTo(Script.From("SET @@foreign_key_checks = 0;")));
+        }
+
+        [Test]
+        public void ComposeScriptDropTables()
+        {
+            //Arrange
+            var tableNames = new[] { "table1", "table2", "table3" };
+
+            //Action
+            var dropTablesScript = _specificDbOperation.ComposeScriptsDropTables(tableNames);
+
+            //Asserts
+            Assert.That(dropTablesScript, Is.SubsetOf(tableNames.Select(table => Script.From(string.Format("DROP TABLE IF EXISTS {0};", table)))));
+            Assert.That(dropTablesScript.ToList(), Has.Count.EqualTo(tableNames.Count()));
+        }
+
+        [Test]
+        public void ComposeScriptDropViews()
+        {
+            //Arrange
+            var viewNames = new[] { "view1", "view2" };
+
+            //Action
+            var dropViewsScript = _specificDbOperation.ComposeScriptsDropViews(viewNames);
+
+            //Asserts
+            Assert.That(dropViewsScript, Is.SubsetOf(viewNames.Select(view => Script.From(string.Format("DROP VIEW IF EXISTS {0};", view)))));
+            Assert.That(dropViewsScript.ToList(), Has.Count.EqualTo(viewNames.Count()));
+        }
+
+        [Test]
+        public void ComposeScriptDropStoredProcedures()
+        {
+            //Arrange
+            var storedProcedureNames = new[] { "sp1", "routine", "storedProcedure" };
+
+            //Action
+            var dropSPsScript = _specificDbOperation.ComposeScriptsDropStoredProcedures(storedProcedureNames);
+
+            //Asserts
+            Assert.That(dropSPsScript, Is.SubsetOf(storedProcedureNames.Select(sp => Script.From(string.Format("DROP PROCEDURE IF EXISTS {0};", sp)))));
+            Assert.That(dropSPsScript.ToList(), Has.Count.EqualTo(storedProcedureNames.Count()));
+        }
+
+        [Test]
+        public void ComposeScriptDropFunctions()
+        {
+            //Arrange
+            var functionNames = new[] { "FUNC1", "FUNCTION", "fnLerele" };
+
+            //Action
+            var dropFuncScript = _specificDbOperation.ComposeScriptsDropFunctions(functionNames);
+
+            //Asserts
+            Assert.That(dropFuncScript, Is.SubsetOf(functionNames.Select(func => Script.From(string.Format("DROP FUNCTION IF EXISTS {0};", func)))));
+            Assert.That(dropFuncScript.ToList(), Has.Count.EqualTo(functionNames.Count()));
         }
     }
 }
